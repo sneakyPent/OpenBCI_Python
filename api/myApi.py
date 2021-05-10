bf96cedb9e4c0f8246089732c8cdb573f7725378
sneakyPent <zaharioudakis@yahoo.com>
2021-05-10 19:10:37 +0300

    create my own api for cyton board

---
+++ api/myApi.py
@@ -0,0 +1,79 @@
+import sys
+import filters
+
+sys.path.append('..')
+from openbci.cyton import *
+
+
+class BoardCytonApi:
+    def __init__(self, board=None, lowerBand=None, upperBand=None, timeWindow=None, samplingFrequency=None):
+        if lowerBand is None:
+            self.lowerBand = 5
+        else:
+            self.lowerBand = lowerBand
+        if upperBand is None:
+            self.upperBand = 50
+        else:
+            self.upperBand = upperBand
+        if timeWindow is None:
+            self.timeWindow = 5
+        else:
+            self.timeWindow = timeWindow
+        if samplingFrequency is None:
+            self.samplingFrequency = 250
+        else:
+            self.samplingFrequency = samplingFrequency
+        if board is None:
+            self.board = None
+        else:
+            self.board = board
+        self.connected = False
+        self.printingDataConsole = True
+        self.data = [[0, 0, 0, 0, 0, 0, 0, 0]]
+        self.filteredData = [[0, 0, 0, 0, 0, 0, 0, 0]]
+        # Define OpenBCI callback function
+
+    def save_data(self, sample):
+        # store the filtered data
+        dt = []
+        dtfl = []
+        for i in range(len(sample.channel_data)):
+            dt.append(sample.channel_data[i] * scale_fac_uVolts_per_count)
+            dtfl.append(filters.bandpass(self.lowerBand, self.upperBand, [dt[i]]).item(0))
+        self.data.append(dt)
+        self.filteredData.append(dtfl)
+        if self.printingDataConsole:
+            print([i for i in np.array(self.data[-1:]).tolist()])
+            print([i for i in np.array(self.filteredData[-1:]).tolist()])
+
+    # Define thread function
+    def stream(self):
+        if not self.connected:
+            print('Not connected board')
+        else:
+            self.board.start_streaming(self.save_data)
+
+    def connect(self):
+        if not self.connected:
+            self.board = OpenBCICyton()
+            self.connected = True
+        else:
+            print('Already have a connection')
+
+    def stop(self):
+        if not self.connected:
+            print('Not connected board')
+        else:
+            self.board.stop()
+
+    def disconnect(self):
+        if not self.connected:
+            print('Not connected board')
+        else:
+            self.board.disconnect()
+            self.connected = False
+
+    def saveDataXls(self):
+        self.printingDataConsole = not self.printingDataConsole
+        # t_data = np.array(data[-fs * timeWindow:]).T
+        # print(t_data)
--- test_log.py
+++ test_log.py
@@ -3,6 +3,7 @@ import time
 import logging
 from openbci import cyton as bci
 import sys
+import pdb
 
 sys.path.append('..')  # help python find cyton.py relative to scripts folder
 
@@ -17,19 +18,21 @@ def printData(sample):
 
 
 if __name__ == '__main__':
-    port = '/dev/tty.usbserial-DN0096XA'
+    port = '/dev/ttyUSB1'
     baud = 115200
     logging.basicConfig(filename="test.log", format='%(message)s', level=logging.DEBUG)
     logging.info('---------LOG START-------------')
     board = bci.OpenBCICyton(port=port, scaled_output=False, log=True)
 
     # 32 bit reset
+    pdb.set_trace()
     board.ser.write('v')
     time.sleep(0.100)
+    pdb.set_trace()
 
-    # connect pins to vcc
-    board.ser.write('p')
-    time.sleep(0.100)
+    # # connect pins to vcc
+    # board.ser.write('p')
+    # time.sleep(0.100)
 
-    # board.start_streaming(printData)
-    board.print_packets_in()
+    # # board.start_streaming(printData)
+    # board.print_packets_in()
